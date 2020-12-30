4436386fefcda45468a5a91c9448c8568d64d09e
Louis-Joseph Fournier <lj.fournier@ateme.com>
2020-12-30 16:37:44 +0100

    Stream object to manage lengths

--- src/read_grib2.cpp
+++ src/read_grib2.cpp
@@ -23,7 +23,7 @@ namespace {
  * http://www.tecepe.com.br/wiki/index.php?title=NOAAWinds
 */
 
-const bool debug = false;
+const bool debug = true;
 
 struct Datetime {
     int year, month, day;
@@ -46,124 +46,174 @@ struct Message {
     Grid grid;
 };
 
+class Stream {
+public:
+    Stream(istream& fin) : fin(fin) {
+        sectionId = sectionLen = sectionRemain = -1;
+    }
+
+    bool read(int len) {
+        if (len > int(sizeof(data)))
+            return false;
+
+        if (len > sectionRemain && sectionLen >= 0)
+            return false;
+
+        fin.read(data, len);
+        if (!fin) {
+            cerr << "Cannot read " << len << " bytes" << endl;
+            return false;
+        }
+
+        sectionRemain -= len;
+        return true;
+    }
+
+    bool sectionBegin(int maxLen) {
+        sectionId = -1;
+        sectionLen = -1;
+
+        if (maxLen < 4)
+            return false;
+
+        if (!read(4))
+            return false;
+
+        if (data[0] == '7' && data[1] == '7' && data[2] == '7' && data[3] == '7') {
+            sectionId = 7; // end section
+            sectionLen = 4;
+            sectionRemain = 0;
+            return true;
+        }
+
+        sectionLen = min<int>(len32(), maxLen);
+        sectionRemain = sectionLen - 4;
+
+        if (sectionRemain <= 0 || !read(1))
+            return false;
+
+        sectionId = data[0];
+
+        return true;
+    }
+
+    bool sectionEnd() {
+        fin.seekg(sectionRemain, ios_base::cur);
+        return !fin.eof();
+    }
+
+    uint16_t len16() const {
+        return ::len16(data);
+    }
+
+    uint32_t len32() const {
+        return ::len32(data);
+    }
+
+    uint64_t len64() const {
+        return ::len64(data);
+    }
+
+    char data[64];
+    int sectionId;
+    istream& fin;
+    int sectionLen;
+    int sectionRemain;
+};
+
 bool error(const char *msg)
 {
     cerr << "read_grib2 error: " << msg << endl;
     return false;
 }
 
-bool readFile(istream& fin, char *data, int len, const char *title = nullptr)
+#define STREAM(n) if (!stream.read(n)) return false
+
+bool readIndicatorSection(Stream& stream, Message& message)
 {
-    fin.read(data, len);
-
-    if (!fin) {
-        cerr << "Cannot read " << len << " bytes";
-        if (title)
-            cerr << " for " << title;
-        cerr << endl;
-        return false;
-    }
-
-    return true;
-}
-
-#define READ(n) if (!readFile(fin, data, n)) return false
-
-bool readEndOfSection(istream& fin, int len)
-{
-    fin.seekg(len, ios_base::cur);
-    return !fin.eof();
-}
-
-bool readIndicatorSection(istream& fin, Message& message)
-{
-    char data[8];
+    stream.sectionLen = stream.sectionRemain = 16;
     message.len = 0;
 
-    READ(4);
+    STREAM(4);
 
-    if (strncmp(data, "GRIB", 4))
+    if (strncmp(stream.data, "GRIB", 4))
         return error("Not GRIB header");
 
     // reserved
-    READ(2);
+    STREAM(2);
 
     // discipline
-    READ(1);
+    STREAM(1);
 
-    if (data[0] != 0)
+    if (stream.data[0] != 0)
         return error("Discipline is not meteorological");
 
-
     // edition number
-    READ(1);
+    STREAM(1);
 
-    if (data[0] != 2)
+    if (stream.data[0] != 2)
         return error("Edition number is not 2");
 
     // msg len
-    READ(8);
+    STREAM(8);
 
-    message.len = len64(data);
-    message.lenRead = 16;
+    message.len = stream.len64();
+    message.lenRead = stream.sectionLen;
 
     return true;
 }
 
-bool readIdentificationSection(istream& fin, Message& message, int sectionLen)
+bool readIdentificationSection(Stream& stream, Message& message)
 {
-    char data[7];
-
     // generating center and subcenter
-    READ(4);
+    STREAM(4);
 
     // table version
-    READ(1);
+    STREAM(1);
 
-    if (data[0] != 2)
+    if (stream.data[0] != 2)
         return error("Master table version number is not 2");
 
     // version of local tables
-    READ(1);
-    message.nbLocal = data[0];
+    STREAM(1);
+    message.nbLocal = stream.data[0];
 
     // significance of reference time
-    READ(1);
+    STREAM(1);
 
     // datetime
-    READ(7);
+    STREAM(7);
 
-    message.datetime.year = bigendian(*(uint16_t*)data);
-    message.datetime.month = data[2];
-    message.datetime.day = data[3];
-    message.datetime.hour = data[4];
-    message.datetime.minute = data[5];
-    message.datetime.second = data[6];
+    message.datetime.year = bigendian(*(uint16_t*)stream.data);
+    message.datetime.month = stream.data[2];
+    message.datetime.day = stream.data[3];
+    message.datetime.hour = stream.data[4];
+    message.datetime.minute = stream.data[5];
+    message.datetime.second = stream.data[6];
 
     // production status
-    READ(1);
+    STREAM(1);
 
     // type of processed data
-    READ(1);
-    if (data[0] != 1)
+    STREAM(1);
+    if (stream.data[0] != 1)
         return error("Not forecast data");
 
-    return readEndOfSection(fin, sectionLen - 16);
+    return stream.sectionEnd();
 }
 
-bool readLocalSection(istream& fin, int sectionLen)
+bool readLocalSection(Stream& stream)
 {
-    return readEndOfSection(fin, sectionLen);
+    return stream.sectionEnd();
 }
 
-bool readGridTemplate30(istream& fin, Message& message, int sectionLen)
+bool readGridTemplate30(Stream& stream, Message& message)
 {
-    char data[15];
     Grid& grid = message.grid;
 
     // shape of earth
-    READ(1);
-    switch (data[0]) {
+    STREAM(1);
+    switch (stream.data[0]) {
     case 0:
         grid.earthRadius = 6367470.;
         break;
@@ -175,145 +225,129 @@ bool readGridTemplate30(istream& fin, Message& message, int sectionLen)
     }
 
     // scale factors
-    READ(15);
+    STREAM(15);
 
     // Ni, Nj
-    READ(4);
-    grid.ni = len32(data);
-    READ(4);
-    grid.nj = len32(data);
+    STREAM(4);
+    grid.ni = stream.len32();
+    STREAM(4);
+    grid.nj = stream.len32();
 
     // basic angle and subdivisions
-    READ(8);
+    STREAM(8);
 
     // first latitude and longitude
-    READ(4);
-    grid.la1 = len32(data) / 1000000.;
-    READ(4);
-    grid.lo1 = len32(data) / 1000000.;
+    STREAM(4);
+    grid.la1 = stream.len32() / 1000000.;
+    STREAM(4);
+    grid.lo1 = stream.len32() / 1000000.;
 
     // component flag
-    READ(1);
-    if (data[0] != 48)
+    STREAM(1);
+    if (stream.data[0] != 48)
         return error("Only component flag 48 is supported (inc x and y)");
 
     // last latitude and longitude - false values ?
-    READ(4);
-    grid.la2 = len32(data) / 1000000.;
-    READ(4);
-    grid.lo2 = len32(data) / 1000000.;
+    STREAM(4);
+    grid.la2 = stream.len32() / 1000000.;
+    STREAM(4);
+    grid.lo2 = stream.len32() / 1000000.;
 
     // directions increment
-    READ(8);
+    STREAM(8);
 
     // scanning mode
-    READ(1);
-    if (data[0] != 0)
+    STREAM(1);
+    if (stream.data[0] != 0)
         return error("Scanning mode 0 only is supported");
 
-    return readEndOfSection(fin, sectionLen - 58);
+    return stream.sectionEnd();
 }
 
-bool readGridDefinition(istream& fin, Message& message, int sectionLen)
+bool readGridDefinition(Stream& stream, Message& message)
 {
-    char data[4];
-
     // source of grid definition
-    READ(1);
-    if (data[0] != 0)
+    STREAM(1);
+    if (stream.data[0] != 0)
         return error("Grid definition other than 0 are not implemented");
 
     // number of data points
-    READ(4);
-    int nbDataPts = len32(data);
+    STREAM(4);
+    // int nbDataPts = stream.len32(); // already in data representation
 
     // octets for optional lists
-    READ(1);
+    STREAM(1);
 
     // interpretation of number of points
-    READ(1); // 2 ?
+    STREAM(1); // 2 ?
 
     // grid definitition number
-    READ(2);
+    STREAM(2);
 
-    int gridDefinitionTpl = len16(data);
+    int gridDefinitionTpl = stream.len16();
 
     switch (gridDefinitionTpl) {
     case 0:
-        return readGridTemplate30(fin, message, sectionLen - 9);
+        return readGridTemplate30(stream, message);
     default:
         return error("Only grid definition 0 is supported (equidistant cylindrical)");
     }
 }
 
-bool readProductionDefinition(istream& fin, int sectionLen)
+bool readProductionDefinition(Stream& stream)
 {
-    char data[2];
-
     // number of coords
-    READ(2);
+    STREAM(2);
 
     // product definition template
-    READ(2);
+    STREAM(2);
 
-    return readEndOfSection(fin, sectionLen - 4);
+    return stream.sectionEnd();
 }
 
-bool readDataRepresentation(istream& fin, int sectionLen)
+bool readDataRepresentation(Stream& stream)
 {
-    char data[4];
-    READ(4);
-    int nb = len32(data);
+    STREAM(4);
+    int nb = stream.len32();
 
-    READ(2);
+    STREAM(2);
 
-    cerr << " pts: " << nb << " tpl " << len16(data) << endl;
+    cerr << " pts: " << nb << " tpl " << stream.len16() << endl;
 
-    return readEndOfSection(fin, sectionLen - 6);
+    return stream.sectionEnd();
 }
 
-bool readSection(istream& fin, Message& message)
+bool readSection(Stream& stream, Message& message)
 {
-    char data[4];
-
     // section length
-    READ(4);
+    stream.sectionBegin(message.len - message.lenRead);
+    message.lenRead += stream.sectionLen;
 
-    if (data[0] == '7' && data[1] == '7' && data[2] == '7' && data[3] == '7') {
-        // 7777 : end section
+    if (stream.sectionId == 7) {
         message.complete = true;
         return true;
     }
 
-    int sectionLen = min<int>(len32(data), message.len - message.lenRead);
-
-    // section number
-    READ(1);
-    int sectionId = int(data[0]);
-
     if (debug)
-    cerr << " section " << sectionId << " len " << sectionLen
+    cerr << " section " << stream.sectionId << " len " << stream.sectionLen
          << " remain " << (message.len - message.lenRead)
          << endl;
 
-    message.lenRead += sectionLen;
-    sectionLen -= 5;
-
-    switch (sectionId) {
+    switch (stream.sectionId) {
     case 1:
-        return readIdentificationSection(fin, message, sectionLen);
+        return readIdentificationSection(stream, message);
     case 2:
-        return readLocalSection(fin, sectionLen);
+        return readLocalSection(stream);
     case 3:
-        return readGridDefinition(fin, message, sectionLen);
+        return readGridDefinition(stream, message);
     case 4:
-        return readProductionDefinition(fin, sectionLen);
+        return readProductionDefinition(stream);
     case 5:
-        return readDataRepresentation(fin, sectionLen);
+        return readDataRepresentation(stream);
 
     case 6:
     case 7:
-        return readEndOfSection(fin, sectionLen);
+        return stream.sectionEnd();
 
     default:
         return error("error : unknown section id");
@@ -322,14 +356,16 @@ bool readSection(istream& fin, Message& message)
 
 bool readMessage(istream& fin, Message& message)
 {
+    Stream stream(fin);
+    message.len = 0;
     message.complete = false;
     message.lenRead = 0;
 
-    if (!readIndicatorSection(fin, message))
+    if (!readIndicatorSection(stream, message))
         return false;
 
     while (!message.complete && message.lenRead < message.len) {
-        if (!readSection(fin, message))
+        if (!readSection(stream, message))
             return false;
     }
 
